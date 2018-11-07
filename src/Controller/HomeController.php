0e407996f8afbba4de235e4006bc097cd400fd64
NOLver <nolletv67@gmail.com>
2018-11-07 15:41:19 +0100

    quete3

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nollet
+ * Date: 06/11/18
+ * Time: 16:24
+ */
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return $this->render("home.html.twig");
+    }
+}
