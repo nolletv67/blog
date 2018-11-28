80b5a4abcb3e41a2476a29c3db0d6377641e1205
NOLver <nolletv67@gmail.com>
2018-11-28 14:09:19 +0100

    quete9 : Form-EntityType

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
@@ -20,5 +24,26 @@ class ArticleController extends AbstractController
             $categories[$key] = $category->getArticles();
         }
         return $this->render('article/index.html.twig', ['categories' => $categories]);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("/article/new", name="article_new")
+     */
+    public function add(Request $request) :Response
+    {
+        $article = new Article();
+        $addArticle = $this->createForm(ArticleType::class, $article);
+        $addArticle->handleRequest($request);
+
+        if ($addArticle->isSubmitted() && $addArticle->isValid()) {
+            $articleEntity = $this->getDoctrine()->getManager();
+            $articleEntity->persist($article);
+            $articleEntity->flush();
+
+            return $this->redirectToRoute('blog_list');
+        }
+        return $this->render('article/add.html.twig', ['add'=>$addArticle->createView()]);
     }
 }
